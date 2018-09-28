1a21dc37f52717fe15d50bbc71a4c99fc8cc88aa
afnanturkistani <42974884+afnanturkistani@users.noreply.github.com>
2018-09-29 01:18:23 +0300

    challeng week5

---
+++ js/channels.js
@@ -0,0 +1,7 @@
+
+var yummy ={name:'Yummy',createdOn:new Date(2016, 3, 1), createdBy:'minus.plus.yummy', starresd:Boolean(false),expiresIn: 100, messageCount: 999};
+var sevencontinents ={name:'SevenContinents',createdOn:new Date(2016, 3, 1), createdBy:'minus.plus.yummy', starresd:Boolean(true),expiresIn: 100, messageCount: 999};
+var killerapp ={name:'KillerApp',createdOn:new Date(2016, 3, 1), createdBy:'minus.plus.yummy', starresd:Boolean(false),expiresIn: 100, messageCount: 999};
+var firstpersononmars ={name:'FirstPersonOnMars',createdOn:new Date(2016, 3, 1), createdBy:'minus.plus.yummy', starresd:Boolean(true),expiresIn: 100, messageCount: 999};
+var octoberfest ={name:'Octoberfest',createdOn:new Date(2016, 3, 1), createdBy:'minus.plus.yummy', starresd:Boolean(false),expiresIn: 100, messageCount: 999};
+
